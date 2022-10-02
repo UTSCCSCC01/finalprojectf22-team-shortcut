609cc4f69792d06cce79d7c2de2e7d29959b4a2b
kz4ever <kz4ever@kz4everdeMacBook-Pro.local>
2022-10-02 16:14:14 -0400

    Add login feature -backend

---
+++ backend/routes/login.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+//const Student = require('../schemas/StudentSchema');
+const e = require('express');
+const { default: mongoose } = require('mongoose');
+//const studentSchema = require('../schemas/studentSchema');
+router.use(express.json());
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb+srv://CalebZhang:Zhangkeyuan333@cluster0.lb38qs6.mongodb.net/?retryWrites=true&w=majority";
+
+//Login
+router.post('/login', bodyParser.json(), async(req,res)=>{
+    var email = req.body.email;
+    var password = req.body.pasword;
+  
+        mongo.connect(url, async(err, db) => {
+        var db1 = db.db("ShortCut");
+        var student  = await db1.collection('Student').findOne({password: req.body.password, "email.data": req.body.email});
+        console.log(student);
+        if(student == null){
+            const params = {
+                check:0
+            }
+            res.json(params);
+        }else{
+            const check1 = {
+                check:1
+            };
+
+            // const obj1 = JSON.parse(check1);
+            // const obj2 = JSON.parse(student);
+
+            const params = {
+                ...check1,
+                ...student,
+            }
+            res.json(params);
+         
+        }
+       
+
+        });
+
+
+
+   
+    });
+
+
+
+
+
+module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,64 @@
 import logo from './logo.svg';
-import './App.css';
 
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Form,Input,Button,Modal, DatePicker, Space, version } from "antd";
+import "antd/dist/antd.css";
+import styles from "./App.css";
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
 function App() {
+  const [Modelvisible, setModelVisible] = useState(false);  
+  const show =()=>{
+    setModelVisible(true)
+  }
 
+  const handleCancel = () =>{
+    setModelVisible(false);
+  }
   
 
   return (
+    
     <div className="App">
+      <p className={styles.inputTitle}>单位管理</p>
+      <h1 className={styles.mystyle}>Hello JavaTpoint</h1>  
+       <Modal title="User Account Sign up" visible={Modelvisible}
+        footer={[
+          <Button type="primary" onClick={handleCancel}>Confirm</Button>,
+          <Button onClick={handleCancel}>Cancel</Button>,
+        ]}>
+        <Form >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required:true,
+                  message: 'please enter',
+                },
+             ]}
+           >
+              <Input  placeholder="please enter"  />
+        </Form.Item> 
+        <Form.Item
+              label="Password"
+              name="pw"
+              rules={[
+                {
+                  required:true,
+                  message: 'please enter',
+                },
+             ]}
+           >
+              <Input.Password  placeholder="please enter"  />
+        </Form.Item> 
+      </Form> 
+      </Modal>
+        <Button className={styles.s} type="primary" onClick={show}>Sign up</Button>
+        
         <div>
-          <h1>ShortCut</h1>
+          <h1>Lena</h1>
           <button onClick={onclick}></button>
         </div>
     </div>
