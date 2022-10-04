0cb612a75ad518d84f5268a2a4ef8c014409e965
CalebZhang <64758307+kz4ever@users.noreply.github.com>
2022-10-04 15:33:42 -0400

    Solve Password Compare Problem

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -9,33 +9,51 @@ router.use(express.json());
 var mongo = require('mongodb').MongoClient;
 var url = "mongodb+srv://CalebZhang:Zhangkeyuan333@cluster0.lb38qs6.mongodb.net/?retryWrites=true&w=majority";
 const bcrypt = require('bcrypt');
+
+async function comparePassword(plainPw,hash){
+    const result = await bcrypt.compare(plainPw,hash);
+    return result;
+}
+async function hashPassword(plainPw){
+    const hash = await bcrypt.hash(plainPw,10);
+    return hash;
+}
+
 //Login
 router.post('/login', bodyParser.json(), async(req,res)=>{
     var email = req.body.email;
-    var password = await bcrypt.hash(req.body.pasword,10) ;
-  
+    var password = req.body.password;
+    // console.log(req.body.password);
+    // console.log(password);
+    
         mongo.connect(url, async(err, db) => {
         var db1 = db.db("ShortCut");
-        var student  = await db1.collection('Student').findOne({password: password, "email.data": req.body.email});
-        console.log(student);
+        var student  = await db1.collection('Student').findOne({"email.data": req.body.email});
         if(student == null){
             const params = {
                 check:0
             }
             res.json(params);
         }else{
-            const check1 = {
-                check:1
-            };
-
-            // const obj1 = JSON.parse(check1);
-            // const obj2 = JSON.parse(student);
-
-            const params = {
-                ...check1,
-                ...student,
+            const result = comparePassword(password,student.password);
+            if(result){
+                const check1 = {
+                    check:1
+                };
+    
+                // const obj1 = JSON.parse(check1);
+                // const obj2 = JSON.parse(student);
+    
+                const params = {
+                    ...check1,
+                    ...student,
+                }
+                res.json(params);
+            }else{
+                res.json(params);
             }
-            res.json(params);
+
+           
          
         }
        
