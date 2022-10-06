9f240087ec82fa83915b16d45898d95b9730366c
Alfred Tze-Hong Ha <alfredhongha@gmail.com>
2022-10-05 20:09:17 -0400

    updated user.js with respect to mongoclient

--- shortcut/backend/routes/user.js
+++ shortcut/backend/routes/user.js
@@ -2,19 +2,39 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const Student = require('../schemas/StudentSchema');
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb+srv://CalebZhang:Zhangkeyuan333@cluster0.lb38qs6.mongodb.net/?retryWrites=true&w=majority";
 
 
-router.post('/delete', bodyParser.json(), function(req, res, next) {
-    var id = req.body._id.$oid;
-    // var id = '"$oid":"6336546fe4297bcdbfe06641"';
-    Student.findById(id, function(err, doc) {
-        if (err || !doc) {
-          res.send('fail');
+router.post('/delete', bodyParser.json(), async(req,res)=>{
+    // var id = req.body._id.$oid;
+    // // var id = '"$oid":"6336546fe4297bcdbfe06641"';
+    // Student.findById(id, function(err, doc) {
+    //     if (err || !doc) {
+    //       res.send('fail');
+    //       return;
+    //     }
+    //     Student.findByIdAndRemove(id).exec();
+    //     res.send('success');
+    // })
+
+    
+    mongo.connect(url, async(err, db) => {
+      if (err){
+        res.send('flag 1');
+        return;
+      }
+      var id = req.body._id.$oid;
+      var db1 = db.db("ShortCut");
+      myobj  = await db1.collection('Student').findOne({"email.data": req.body.email.data});
+      if(myobj == null){
+          res.send('flag 2');
           return;
-        }
-        Student.findByIdAndRemove(id).exec();
-        res.send('success');
-    })
+      }
+      await db1.collection('Student').deleteOne({"email.data": req.body.email.data});
+      res.send('success');
+    });
+
 });
 
 module.exports = router;
