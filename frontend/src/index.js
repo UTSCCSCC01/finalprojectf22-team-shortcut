d39dbbeed6763238d66e1e486dc2b5086ff0b5a8
MeganMujiaLiu <88005523+MeganMujiaLiu@users.noreply.github.com>
2022-10-06 22:10:19 -0400

    SHOR-29 Create index.js for render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Link, Routes, Route, BrowserRouter as Router, useNavigate} from "react-router-dom";
+import routes from './routes';
+
+
+
+// pages
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import Logo from "./Components/Logo";
+import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/PersonalProfile";
+
+
+
+const root =ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  routes()
+  
+ 
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
