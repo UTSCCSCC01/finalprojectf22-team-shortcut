589cb9e243aeae8fb5137cf4ed49bf9521b234bc
MeganMujiaLiu <88005523+MeganMujiaLiu@users.noreply.github.com>
2022-10-07 10:05:23 +0800

    SHOR-29 Create routes.js
    add all routes in one file

---
+++ frontend/src/routes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import Logo from "./Components/Logo";
+import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/PersonalProfile";
+
+
+const routes=()=>{
+
+    return(
+        
+        
+        <Router>
+              
+            <Routes>
+                <Route path="/" element={<Login/>} />
+                <Route path ="/signup" element={<Signup/>} />
+                <Route path = "/home" element={<Home/>}/>
+                <Route path = "/profile" element={<Profile/>}/>
+            </Routes>
+        </Router>
+        
+    )  
+    
+    
+}
+
+export default routes;
