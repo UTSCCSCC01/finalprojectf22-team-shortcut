cb5a25aba7f0ab1de7b8514ea7ab6dc40d54ff8d
Yry01 <103943433+Yry01@users.noreply.github.com>
2022-10-26 18:09:14 -0400

    [SHOR-18-2] finish advance search on programs
    version 2

--- shortcut/backend/routes/advanceprograms.js
+++ shortcut/backend/routes/advanceprograms.js
@@ -7,6 +7,10 @@ var mongo = require('mongodb').MongoClient;
 var url = "mongodb+srv://CalebZhang:Zhangkeyuan333@cluster0.lb38qs6.mongodb.net/?retryWrites=true&w=majority";
 const bodyParser = require('body-parser');
 //search 
+//if erolment = "\\w" search limited
+//if erolment = "" search everything
+//if erolment = "unlimited" match the empty string
+//if coop ="nocoop" match no coop
 router.post('/advanceprograms', bodyParser.json(), async(req,res)=>{
     var area = req.body.area;
     var degree = req.body.degree;
@@ -16,18 +20,55 @@ router.post('/advanceprograms', bodyParser.json(), async(req,res)=>{
     // console.log(key);
     mongo.connect(url, async(err, db) => {
         var db1 = db.db("ShortCut");
-        db1.collection('Program').find({area:{$regex:area,$options: 'i'},degree:{$regex:degree,$options: 'i'},
-        enrolment:{$regex:enrolment,$options: 'i'},coop:{$regex:coop,$options: 'i'},type:{$regex:type,$options: 'i'}}).toArray((err,result) =>{
-            if(err){
-                res.send({length:0});
-                return;
-            }
-            console.log(result.length);
-            res.send({length:result.length,result});
+        if(enrolment=="unlimited"&&coop=="nocoop"){
+            db1.collection('Program').find({area:{$regex:area,$options: 'i'},degree:{$regex:degree,$options: 'i'},
+            enrolment:"",coop:"",type:{$regex:type,$options: 'i'}}).toArray((err,result) =>{
+                if(err){
+                    res.send({length:0});
+                    return;
+                }
+                console.log(result.length);
+                res.send({length:result.length,result});
 
-        });
-        // console.log(db1.collection('Program').find({name:{$regex:key}}, {name:1, type:1}));
-        // res.json(db1.collection('Program').find({name:{$regex:key}}, {name:1, type:1}));
+            });
+        }
+        else if((enrolment=="unlimited"&&coop!="nocoop")){
+            db1.collection('Program').find({area:{$regex:area,$options: 'i'},degree:{$regex:degree,$options: 'i'},
+            enrolment:"",coop:{$regex:coop,$options: 'i'},type:{$regex:type,$options: 'i'}}).toArray((err,result) =>{
+                if(err){
+                    res.send({length:0});
+                    return;
+                }
+                console.log(result.length);
+                res.send({length:result.length,result});
+
+            });
+        }
+        else if((enrolment!="unlimited"&&coop!="nocoop")){
+            db1.collection('Program').find({area:{$regex:area,$options: 'i'},degree:{$regex:degree,$options: 'i'},
+            enrolment:{$regex:enrolment,$options: 'i'},coop:{$regex:coop,$options: 'i'},type:{$regex:type,$options: 'i'}}).toArray((err,result) =>{
+                if(err){
+                    res.send({length:0});
+                    return;
+                }
+                console.log(result.length);
+                res.send({length:result.length,result});
+
+            });
+        }
+        else{
+            db1.collection('Program').find({area:{$regex:area,$options: 'i'},degree:{$regex:degree,$options: 'i'},
+            enrolment:{$regex:enrolment,$options: 'i'},coop:"",type:{$regex:type,$options: 'i'}}).toArray((err,result) =>{
+                if(err){
+                    res.send({length:0});
+                    return;
+                }
+                console.log(result.length);
+                res.send({length:result.length,result});
+
+            });
+        }
+
     });
 });
 module.exports = router;
