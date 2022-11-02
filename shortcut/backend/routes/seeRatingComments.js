cbf1dca80ac97c9a0d4df5f99d3f2e7cf80b35b6
Alfred Tze-Hong Ha <alfredhongha@gmail.com>
2022-11-02 17:37:06 -0400

    added feature-SHOR-11-3 (backend for SHOR 11), the feature for displaying child comments of a parent comment

---
+++ shortcut/backend/routes/seeRatingComments.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const e = require('express');
+const { default: mongoose } = require('mongoose');
+//const studentSchema = require('../schemas/studentSchema');
+router.use(express.json());
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb+srv://CalebZhang:Zhangkeyuan333@cluster0.lb38qs6.mongodb.net/?retryWrites=true&w=majority";
+
+
+
+router.post('/seeRatingComments', bodyParser.json(), async (req, res) => {
+    var key = mongoose.Types.ObjectId(req.body._id);
+    mongo.connect(url, async(err, db) => {
+        if (err){
+          res.json({ 'result': 0, 'message': 'Error.' });
+          res.end();
+          return;
+        }
+        var db1 = db.db("ShortCut");
+        db1.collection('Comment').find({"parent":{ $eq: key }}).toArray((err,child_comments) =>{
+            if (err){
+                res.json({ 'result': 0, 'message': 'Error.' });
+                res.end();
+                return;
+            }
+            res.json({ 'result': 1, 'child comments': child_comments, 'message': 'returning child comments.' });
+            res.end(); 
+
+            db.close();
+            
+        });
+        
+    });
+
+});
+module.exports = router;
