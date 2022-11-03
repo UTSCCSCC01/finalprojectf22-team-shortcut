c1176f5e1f4b0d53dc723e7c2b352ad1056cc351
Chau Nguyen <96738596+chaunguyenm@users.noreply.github.com>
2022-11-02 20:03:51 -0400

    feature-SHOR-8-3 (#29)
    * SHOR-8-2 updated CourseSchema
    
    added field score to CourseSchema.js, score: {average, num}
    
    * SHOR-8-2 added endpoint to update Course
    
    added /course/update/course to add new field score to all existing documents of Course collection in database
    
    * SHOR-8-3 updated /course/rate to update score field
    
    - removed username field from RatingSchema and CommentSchema
    - updated /course/rate endpoint to update score for a course based on new user ratings
    
    * SHOR-8-3 updated /seeCourseRatings to return names
    
    RatingSchema and CommentSchema no longer store usernames, /seeCourseRatings returns all rating, comments, and corresponding usernames
    
    * SHOR-8-3 added default values for CourseSchema
    
    score is defaulted to {average: 6, num: 0}

--- shortcut/backend/routes/seeCourseRatings.js
+++ shortcut/backend/routes/seeCourseRatings.js
@@ -13,6 +13,9 @@ var url = "mongodb+srv://CalebZhang:Zhangkeyuan333@cluster0.lb38qs6.mongodb.net/
 router.post('/seeCourseRatings', bodyParser.json(), async (req, res) => {
     var key = req.body.course;
     var ids = [];
+    var student;
+    var name;
+    var names = [];
     mongo.connect(url, async(err, db) => {
         if (err){
           res.json({ 'result': 0, 'message': 'Error.' });
@@ -20,7 +23,7 @@ router.post('/seeCourseRatings', bodyParser.json(), async (req, res) => {
           return;
         }
         var db1 = db.db("ShortCut");
-        db1.collection('Rating').find({"course":{ $eq: key }}).toArray((err,ratings) =>{
+        db1.collection('Rating').find({"course":{ $eq: key }}).toArray(async (err,ratings) =>{
             if (err){
                 res.json({ 'result': 0, 'message': 'Error.' });
                 res.end();
@@ -28,6 +31,14 @@ router.post('/seeCourseRatings', bodyParser.json(), async (req, res) => {
             }
             for(const rating of ratings){
                 ids.push(mongoose.Types.ObjectId(rating.comment));
+                student = await db1.collection('Student').findOne({"email.data": { $eq: rating.email}});
+                if(student == null){
+                    name = "deleted user";
+                }
+                else{
+                    name = student.name.data;
+                }
+                names.push(name);
             }
             db1.collection('Comment').find({"_id":{ $in: ids }}).toArray((err,comments) =>{
                 if (err){
@@ -35,7 +46,7 @@ router.post('/seeCourseRatings', bodyParser.json(), async (req, res) => {
                     res.end();
                     return;
                 }
-                res.json({ 'result': 1, 'ratings': ratings, 'comments': comments, 'message': 'returning course ratings.' });
+                res.json({ 'result': 1, 'ratings': ratings, 'comments': comments, 'names': names, 'message': 'returning course ratings.' });
                 res.end(); 
 
                 db.close();
