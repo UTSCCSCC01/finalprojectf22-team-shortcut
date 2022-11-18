a80d6561572567931489aead274127ec8a218646
MeganMujiaLiu <88005523+MeganMujiaLiu@users.noreply.github.com>
2022-11-17 23:14:08 -0500

    dev testing fix bug in components folder (#52)

--- shortcut/frontend/src/Components/Navbar.js
+++ shortcut/frontend/src/Components/Navbar.js
@@ -1,21 +1,43 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 // icons
 import { BsPersonCircle } from "react-icons/bs";
 
 import Logo from "../Components/Logo";
 import Button from "../Components/Button";
-import {Avatar} from '@mui/material';
+import {Avatar, Box, Switch, Paper, FormControlLabel} from '@mui/material';
+
+import {light, dark} from "./Themes";
+import {ThemeProvider} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 
-const Navbar =({toProfile, toHome})=>{
 
+const Navbar =({toProfile, toHome, sendState})=>{
+
+    
+    const [mode, setMode]=useState(JSON.parse(localStorage.getItem('mode'))|| false);
+    const changeMode=()=>{
+       
+        setMode(!mode);
+        sendState();
+
+    }
+    useEffect(()=>{
+        localStorage.setItem('mode', JSON.stringify(mode), [mode])
+    })
     
 
     return(
-        <div style={{backgroundColor: "ghostwhite", gap:"45em", display: "flex",flexDirection: "row"}}>
+        <ThemeProvider theme={mode ? dark:light}>
+            <CssBaseline/>
+        <Box sx={{bgcolor:"background.paper.third"}} style={{ gap:"45em", display: "flex",flexDirection: "row"}}>
             <Logo/>
             <div style={{gap: "2em", display: "flex",flexDirection: "row"}}>
+                <FormControlLabel label="Dark Mode" control ={<Switch onChange={changeMode} checked={mode} />}/>
+                
+
                 <Button func={toHome} text="Home" col="steelblue"/>
             
                 <BsPersonCircle onClick={toProfile} size="50"  />
@@ -23,7 +45,7 @@ const Navbar =({toProfile, toHome})=>{
             </div>
             
             
-        </div>
+        </Box></ThemeProvider>
         
 
     )
